8f2c3f337b2290928392a7c5e292f16d8cb2622e
Amoreemorte <foxroit45@gmail.com>
2025-05-11 16:43:46 +0300

    Create RPC_client.go
    Create simple rpc client

---
+++ simple RPC/client/RPC_client.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/rpc"
+)
+
+func main() {
+	client, err := rpc.Dial("tcp", ":8080")
+	if err != nil {
+		log.Fatal(err)
+	}
+	request := []string{"First message"}
+	var responce string
+
+	err = client.Call("Server.Send", request, &responce)
+	if err != nil {
+		log.Print(err)
+	}
+	err = client.Call("Server.Send", []string{"Second message"}, &responce)
+	if err != nil {
+		log.Print(err)
+	}
+
+	err = client.Call("Server.Messages", 1, &request)
+	if err != nil {
+		log.Print(err)
+	}
+
+	fmt.Printf("%v", request)
+}
