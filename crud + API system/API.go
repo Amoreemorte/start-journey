ed42cdf9745fee4d758a0650db36ea615265758e
Amoreemorte <foxroit45@gmail.com>
2025-05-17 15:20:26 +0300

    Update logMiddleware
    add simple logging  with RequestUri and http Method

--- crud + API system/API.go
+++ crud + API system/API.go
@@ -25,6 +25,7 @@ func (api *API) RegisterEndpoints() {
 
 func logMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Println(r.RequestURI, r.Method)
 		next.ServeHTTP(w, r)
 	})
 }
